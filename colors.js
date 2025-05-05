b6f7f381e89fd5121faafd5e0d16f77e029c69ff
Caio <101003481+Caiozzz@users.noreply.github.com>
2025-05-05 19:07:32 -0300

    Deleção de arquivo duplicado

--- colors.js
+++
@@ -1,6 +0,0 @@
-const inputColor = document.querySelector('.color');
-const appCode = document.querySelector('.app-code');
-
-inputColor.addEventListener('change', () => {
-   appCode.style.backgroundColor = inputColor.value;
-})
